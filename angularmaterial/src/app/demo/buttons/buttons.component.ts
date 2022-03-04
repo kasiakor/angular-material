75a6d28377c3138252eea8b5a9cebeb507cd9334
kasia.kordys <kasia.kordys@gmail.com>
2022-03-04 15:59:43 +0200

    create demo module

---
+++ angularmaterial/src/app/demo/buttons/buttons.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-buttons',
+  template: `
+    <p>
+      buttons works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class ButtonsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
