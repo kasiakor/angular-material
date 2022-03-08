ae7d40f69b9e21de1bf4b9fcb4f331b60dedd6ad
kasia.kordys <kasia.kordys@gmail.com>
2022-03-08 17:04:58 +0200

    add main-content and sidenav component

--- angularmaterial/src/app/contactmanager/contactmanager.module.ts
+++ angularmaterial/src/app/contactmanager/contactmanager.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContactmanagerAppComponent } from './contactmanager-app.component';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
+import { MainContentComponent } from './components/main-content/main-content.component';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
 
 
 
 @NgModule({
   declarations: [
     ContactmanagerAppComponent,
-    ToolbarComponent
+    ToolbarComponent,
+    MainContentComponent,
+    SidenavComponent
   ],
   imports: [
     CommonModule
