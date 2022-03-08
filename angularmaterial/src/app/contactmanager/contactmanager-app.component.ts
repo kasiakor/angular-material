f4c48b298f618a957bd1629ee8479dd8ab1bd3b3
kasia.kordys <kasia.kordys@gmail.com>
2022-03-08 16:59:38 +0200

    create contact manager module and component

---
+++ angularmaterial/src/app/contactmanager/contactmanager-app.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contactmanager-app',
+  template: `
+    <p>
+      contactmanager-app works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class ContactmanagerAppComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ angularmaterial/src/app/contactmanager/contactmanager.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ContactmanagerAppComponent } from './contactmanager-app.component';
+
+
+
+@NgModule({
+  declarations: [
+    ContactmanagerAppComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class ContactmanagerModule { }
