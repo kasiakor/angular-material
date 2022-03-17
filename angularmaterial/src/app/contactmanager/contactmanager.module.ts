822dcb283cc69580b9a76805719819ab92c318c4
kasia.kordys <kasia.kordys@gmail.com>
2022-03-17 16:48:20 +0200

    define contact manager module

--- angularmaterial/src/app/contactmanager/contactmanager.module.ts
+++ angularmaterial/src/app/contactmanager/contactmanager.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Routes, RouterModule } from '@angular/router';
+
+
+import { MaterialModule } from '../shared/material.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule } from '@angular/forms';
+
 import { ContactmanagerAppComponent } from './contactmanager-app.component';
 import { ToolbarComponent } from './components/toolbar/toolbar.component';
 import { MainContentComponent } from './components/main-content/main-content.component';
 import { SidenavComponent } from './components/sidenav/sidenav.component';
 
 
-
+const routes: Routes = [
+  { path: 'contactmanager', loadChildren: () => import('../contactmanager/contactmanager.module').then(m => m.ContactmanagerModule)},
+  { path: 'demo', loadChildren: () => import('../demo/demo.module').then(m => m.DemoModule)},
+  { path: '**', redirectTo: 'contactmanager'}
+];
 @NgModule({
   declarations: [
     ContactmanagerAppComponent,
@@ -15,7 +26,11 @@ import { SidenavComponent } from './components/sidenav/sidenav.component';
     SidenavComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MaterialModule,
+    FlexLayoutModule,
+    FormsModule,
+    RouterModule.forChild(routes)
   ]
 })
 export class ContactmanagerModule { }
